5535d8b603df63b9c60d4bfaf2e20b06ea1c7a10
muhasyraf <faizkamil39@gmail.com>
2024-01-12 10:17:58 +0700

    minor update: theme, style, layoute

--- src/App.tsx
+++ src/App.tsx
@@ -98,27 +98,59 @@ function App() {
     setGamePlay(newAct);
   };
 
+  const resetScores = () => {
+    resetGame();
+    setScores(SCORES_STARTER);
+    setTimeout(() => {
+      window.alert("Scores resetted");
+    }, 100);
+  };
+
   const playerColor =
     currentPlayer === "X" ? "text-lime-300" : "text-fuchsia-300";
 
   return (
     <div
-      className="h-full sm:h-screen p-4 bg-cyan-950 text-cyan-100 flex flex-col items-center sm:grid sm:grid-rows-1 sm:grid-cols-3 gap-2"
+      className="h-full sm:h-screen p-4 text-fun-pink-light flex flex-col items-center sm:grid sm:grid-rows-1 sm:grid-cols-3 gap-2"
       id="bground"
     >
-      <div className="sm:justify-self-center text-3xl flex flex-col justify-center">
-        <h3 className="font-display">Scores</h3>
-        <p>
-          <span className="text-lime-300 font-display">X:</span>{" "}
-          <span>{scores["X"]}</span>
-        </p>
-        <p>
-          <span className="text-fuchsia-300 font-display">O:</span>{" "}
-          <span>{scores["O"]}</span>
-        </p>
+      <div className="sm:justify-self-center text-2xl sm:text-3xl max-sm:items-center grid grid-cols-2 sm:flex sm:flex-col sm:gap-2 justify-center max-sm:order-last">
+        <div>
+          <h3 className="font-display">Scores</h3>
+          <p>
+            <span className="text-lime-300 font-display">X:</span>{" "}
+            <span>{scores["X"]}</span>
+          </p>
+          <p>
+            <span className="text-fuchsia-300 font-display">O:</span>{" "}
+            <span>{scores["O"]}</span>
+          </p>
+        </div>
+        <div className="text-xl">
+          <button
+            type="button"
+            className="bg-transparent border-2 border-fun-pink text-fun-pink-light font-display px-4 py-2 rounded-xl hover:bg-fun-pink-light hover:border-transparent hover:text-bg"
+            onClick={resetScores}
+          >
+            Reset Scores
+          </button>
+        </div>
       </div>
-      <div className="sm:col-span-2 sm:justify-self-start">
-        <h1 className="text-5xl text-center font-display">Miltactoe</h1>
+      <div className="sm:col-span-2 sm:justify-self-start max-sm:p-2">
+        <h1 className="text-4xl sm:text-5xl text-center font-display">
+          Kamil-Tac-Toe
+        </h1>
+        <p className="mt-1 text-center font-display text-xl sm:text-2xl">
+          A game by{" "}
+          <a
+            href="https://muhasyraf.github.io"
+            rel="noopener"
+            target="_blank"
+            className="text-fun-pink hover:text-fun-pink-light"
+          >
+            Asyraf
+          </a>
+        </p>
         <div className="mt-8 grid grid-cols-3 gap-3 mx-auto w-96">
           {gamePlay.map((player, playerKey) => (
             <Board
@@ -128,11 +160,12 @@ function App() {
             />
           ))}
         </div>
-        <p className="mt-2 font-display text-center text-3xl">
-          Next Player:{" "}
+        <p className="mt-2 font-display text-center text-xl sm:text-3xl">
+          Let's go{" "}
           <span className={`font-display ${playerColor} `}>
-            {currentPlayer}
+            {currentPlayer}!{" "}
           </span>
+          This is your turn
         </p>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        bg: "#29201b",
+        "fun-gray-light": "#c0daed",
+        "fun-gray": "#7b89a8",
+        "fun-gray-medium": "#767c85",
+        "fun-gray-darker": "#2a2a2c",
+        "fun-gray-dark": "#1F1F20",
+        "fun-gray-darkest": "#141414",
+        "fun-pink": "#7fd1eb",
+        "fun-pink-darker": "#0f1b1f",
+        "fun-pink-darkest": "#080f12",
+        "fun-pink-dark": "#246c82",
+        "fun-pink-light": "#c7f2ff",
+      },
       fontFamily: {
         display: ["Rowdies", ...defaultTheme.fontFamily.sans],
       },
